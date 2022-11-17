a30d916310489d295aa6af33350e57d0664a7e34
mahiuddinTuhin <mahiuddiinntuhin@gmail.com>
2022-11-17 10:39:46 +0600

    readme updating

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # zlofy-server
+
+### Technology used on making this server:
+
+    1. NODEJS
+    2. EXPRESS
+    3. MONGOOSE
+    4. DOTENV
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require("./app");
 const port = process.env.PORT;
+
 app.listen(port, () => {
-    console.log(`Server active on port: ${port}`)
-} );
+  console.log(`Server active on port: ${port}`);
+});
